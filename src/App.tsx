e6a87ca81cf10ffa9ab0a2a03a9b4b8d5240174b
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-05 12:12:47 +0900

    feat: react-hook-form validation 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react'
 import { useForm } from 'react-hook-form'
 
 type Gender = 'male' | 'female' | 'other'
@@ -5,11 +6,16 @@ type Gender = 'male' | 'female' | 'other'
 type FormType = {
   name: string
   password: string
+  passwordConfirm: string
   gender: Gender
 }
 
 function App() {
-  const { register, handleSubmit } = useForm<FormType>()
+  const { register, handleSubmit, watch } = useForm<FormType>()
+
+  const password = useRef<string>()
+
+  password.current = watch('password')
 
   function onSubmit(data: FormType) {
     console.log(data)
@@ -19,13 +25,33 @@ function App() {
     <main>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div>이름</div>
-        <input {...register('name')} />
+        <input
+          {...register('name', { required: true, minLength: 2, maxLength: 10 })}
+        />
 
         <div>비밀번호</div>
-        <input type='password' {...register('password')} />
+        <input
+          type='password'
+          {...register('password', {
+            required: true,
+            minLength: 6,
+            maxLength: 12,
+            pattern: /^[a-zA-Z0-9]*$/,
+          })}
+        />
+
+        <div>비밀번호 확인</div>
+        <input
+          type='password'
+          {...register('passwordConfirm', {
+            required: true,
+            validate: (value) => value === password.current,
+          })}
+        />
 
         <div>성별</div>
-        <select {...register('gender')} defaultValue='other'>
+        <select {...register('gender', { required: true })}>
+          <option value=''>성별 선택</option>
           <option value='male'>남성</option>
           <option value='female'>여성</option>
           <option value='other'>미공개</option>
