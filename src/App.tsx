084409cfcf69e434b8c09fa1226ba7f3410036c5
kim-elijah-sol <kimelijahsol@gmail.com>
2024-02-05 11:54:02 +0900

    feat: react-hook-form 기본기

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,42 @@
+import { useForm } from 'react-hook-form'
+
+type Gender = 'male' | 'female' | 'other'
+
+type FormType = {
+  name: string
+  password: string
+  gender: Gender
+}
+
 function App() {
-  return <main></main>
+  const { register, handleSubmit } = useForm<FormType>()
+
+  function onSubmit(data: FormType) {
+    console.log(data)
+  }
+
+  return (
+    <main>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>이름</div>
+        <input {...register('name')} />
+
+        <div>비밀번호</div>
+        <input type='password' {...register('password')} />
+
+        <div>성별</div>
+        <select {...register('gender')} defaultValue='other'>
+          <option value='male'>남성</option>
+          <option value='female'>여성</option>
+          <option value='other'>미공개</option>
+        </select>
+
+        <div>
+          <button type='submit'>제출</button>
+        </div>
+      </form>
+    </main>
+  )
 }
 
 export default App
